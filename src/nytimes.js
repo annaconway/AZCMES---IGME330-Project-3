24a7bc2670bab8cfbd0e068f2be9a461f8da4b69
MatthewSissom <matthewesissom@gmail.com>
2020-12-06 00:26:10 -0500

    added switching between national and world

--- src/nytimes.js
+++ src/nytimes.js
@@ -1,8 +1,20 @@
 import * as ajax from './ajax.js'
 
 const nytURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
-const fliterStart = "&fq=section_name:(\"World\") AND pub_date:("
+const fliterStart = "&fq=section_name:(\"";
+const filterMid = "\") AND pub_date:(";
 const key = "&api-key=8iYDdnGGzxrl05WTA0GZWNdKMJjE3I5O";
+let newsType = "World";
+
+function useNational()
+{
+    newsType = "National";
+}
+
+function useWorld()
+{
+    newsType = "World";
+}
 
 function getWorldEventText(date, callback) {
     function formatArticleInfo(json) {
@@ -21,7 +33,7 @@ function getWorldEventText(date, callback) {
         }
         callback(articles,links);
     }
-    ajax.getJSON(nytURL + fliterStart + date + ")" + key, formatArticleInfo);
+    ajax.getJSON(nytURL + fliterStart + newsType + filterMid + date + ")" + key, formatArticleInfo);
 }
 
-export{getWorldEventText}
+export{getWorldEventText, useNational, useWorld}
